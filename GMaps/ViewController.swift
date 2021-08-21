e447a331ff6e4b888b530f46934cdb3257ab0a49
Konstantin <syancheg@gmail.com>
2021-08-21 14:06:31 +0300

    HomeWork 1

--- GMaps/ViewController.swift
+++ GMaps/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLocationManager()
+        checkLocationStatus()
     }
+    
+    func checkLocationStatus(){
+        let locationStatus = locationManager.authorizationStatus
+        switch locationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("Location access denied")
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    func addMarker(at coordinate: CLLocationCoordinate2D) {
+        let marker = GMSMarker(position: coordinate)
+        marker.map = mapView
+    }
+    
+    private func setupLocationManager(){
+        locationManager.delegate = self
+    }
+}
 
-
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationStatus()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let position = GMSCameraPosition(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 17)
+            mapView.animate(to: position)
+            let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            addMarker(at: coordinate)
+        }
+    }
+    
 }
 
